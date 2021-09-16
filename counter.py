05519827b083c3ac03c80befe7bb657a236d0604
Max Thelen <89579326+maxthelen@users.noreply.github.com>
2021-09-16 09:31:18 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,13 +1,19 @@
+#counter to keep track of shots and score for golf
+#asking user to define number of holes
 num_holes = int(input("Enter number of holes: "))
-total_shots = 0
-total_score = 0
+#initialising arrays to store all shots and scores and pars for the pars
 score = [0] * num_holes
 shots = [0] * num_holes
+pars = [0] * num_holes
+total_shots = 0
+total_score = 0
+
 
 allerton = [4, 4, 4, 3, 4, 4, 4, 3, 3, 4, 4, 3, 4, 4, 4, 4, 3]
-bowring = [4, 3, 4, 4, 4, 4, 4, 4, 4]
-course = allerton
-course_par = sum(allerton)
+bowring = [4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 4, 4, 4, 4, 4]
+
+pars = allerton
+course_par = sum(pars)
 
 i = 0
 
@@ -16,16 +22,14 @@ while i < num_holes:
     print("Hole " + str(hole_num))
 
     shots[i] = int(input("Enter shots for Hole " + str(hole_num) + ": "))
-    score[i] = shots[i] - course[i]
+    score[i] = shots[i] - pars[i]
     total_shots = sum(shots)
     total_score = sum(score)
+
     if total_score > 0:
         print("Total Shots: " + str(total_shots) + " (" + "+" + str(total_score) + ")" )
     if total_score < 1:
         print("Total Shots: " + str(total_shots) + " (" + str(total_score) + ")" )
 
     print("")
-
     i = i + 1
-
-
